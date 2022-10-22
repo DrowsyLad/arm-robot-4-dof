4346bcfeadeb064cfa200970e7cc590c6e41a325
DrowsyLad <tsumwashere@student.ub.ac.id>
2022-10-22 16:03:58 +0700

    Update servo param

--- kimbot_arm.ino
+++ kimbot_arm.ino
@@ -17,14 +17,40 @@ float servo_1_offset_angle = 90, servo_2_offset_angle = 105, servo_3_offset_angl
 
 
 enum servo_param{
+  pin_pwm,
+  deg_range,
   min_pwm,
   max_pwm,
 };
 
-//servo[4]
+int servo_[4][];
+
+servo[1][servo_param::pin_pwm] = 10;
+servo[1][servo_param::deg_range] = 180;
+servo[1][servo_param::min_pwm] = 540;
+servo[1][servo_param::max_pwm] = 2400;
+
+servo[2][servo_param::pin_pwm] = 9;
+servo[2][servo_param::deg_range] = 90;
+servo[2][servo_param::min_pwm] = 540;
+servo[2][servo_param::max_pwm] = 1600;
+
+servo[3][servo_param::pin_pwm] = 6;
+servo[3][servo_param::deg_range] = 90;
+servo[3][servo_param::min_pwm] = 540;
+servo[3][servo_param::max_pwm] = 1600;
+
+servo[4][servo_param::pin_pwm] = 7;
+servo[4][servo_param::deg_range] = 60;
+servo[4][servo_param::min_pwm] = 540;
+servo[4][servo_param::max_pwm] = 1500;
 
 void set_pwm(Servo *servo_target, int output_pwm){
   servo_target->writeMicroseconds(output_pwm);
+}
+
+void move_servo_pwm(float servo_1_target_angle, float servo_2_target_angle, float servo_3_target_angle, float servo_4_target_angle){
+  
 }
 
 void move_servo(float servo_1_target_angle, float servo_2_target_angle, float servo_3_target_angle, float servo_4_target_angle){
@@ -55,46 +81,61 @@ void setup() {
   move_servo(0, 0, 0, 0);
 }
 
-void loop(){ 
+void loop(){
+  move_servo(0, 90, 90, 60);
+  delay(2000);
+  move_servo(0, 0, 0, 0);
+  delay(2000);
+}
+
+void loop_(){ 
 //  set_pwm(&servo2, 750);
-  for(int pwm = 540; pwm < 2400; pwm += 10){ //0->180
-    set_pwm(&servo1, pwm);
-    Serial.println(pwm);
-    delay(10);
-  }
-  for(int pwm = 1600; pwm > 750; pwm -= 10){ //forward
-    set_pwm(&servo2, pwm);
-    Serial.println(pwm);
-    delay(10);
-  }
-  for(int pwm = 540; pwm < 1200; pwm += 10){ //open
-    set_pwm(&servo4, pwm);
-    Serial.println(pwm);
-    delay(10);
-  }
-  for(int pwm = 750; pwm < 1600; pwm += 10){ //backward
-    set_pwm(&servo2, pwm);
-    Serial.println(pwm);
-    delay(10);
-  }
-  for(int pwm = 2400; pwm > 540; pwm -= 10){ //180->0
-    set_pwm(&servo1, pwm);
-    Serial.println(pwm);
-    delay(10);
-  }
-  for(int pwm = 1600; pwm > 750; pwm -= 10){ //forward
-    set_pwm(&servo2, pwm);
-    Serial.println(pwm);
-    delay(10);
-  }
-  for(int pwm = 1200; pwm > 540; pwm -= 10){ //close
-    set_pwm(&servo4, pwm);
-    Serial.println(pwm);
-    delay(10);
-  }
-  for(int pwm = 750; pwm < 1600; pwm += 10){ //backward
-    set_pwm(&servo2, pwm);
-    Serial.println(pwm);
-    delay(10);
-  }
+  // move_servo(90, 0, 0, 0);
+  // for(int pwm = 540; pwm < 2400; pwm += 10){ //0->180
+  //   set_pwm(&servo1, pwm);
+  //   Serial.println(pwm);
+  //   delay(10);
+  // }
+  move_servo(90, 90, 90, 0);
+  // for(int pwm = 1600; pwm > 750; pwm -= 10){ //forward
+  //   set_pwm(&servo2, pwm);
+  //   Serial.println(pwm);
+  //   delay(10);
+  // }
+  move_servo(90, 90, 90, 60);
+  // for(int pwm = 540; pwm < 1200; pwm += 10){ //open
+  //   set_pwm(&servo4, pwm);
+  //   Serial.println(pwm);
+  //   delay(10);
+  // }
+  move_servo(-90, 0, 0, 60);
+  // for(int pwm = 750; pwm < 1600; pwm += 10){ //backward
+  //   set_pwm(&servo2, pwm);
+  //   Serial.println(pwm);
+  //   delay(10);
+  // }
+  // move_servo(-90, 0, 0, 60);
+  // for(int pwm = 2400; pwm > 540; pwm -= 10){ //180->0
+  //   set_pwm(&servo1, pwm);
+  //   Serial.println(pwm);
+  //   delay(10);
+  // }
+  move_servo(-90, 90, 90, 60);
+  // for(int pwm = 1600; pwm > 750; pwm -= 10){ //forward
+  //   set_pwm(&servo2, pwm);
+  //   Serial.println(pwm);
+  //   delay(10);
+  // }
+  move_servo(-90, 90, 90, 0);
+  // for(int pwm = 1200; pwm > 540; pwm -= 10){ //close
+  //   set_pwm(&servo4, pwm);
+  //   Serial.println(pwm);
+  //   delay(10);
+  // }
+  move_servo(90, 0, 0, 0);
+  // for(int pwm = 750; pwm < 1600; pwm += 10){ //backward
+  //   set_pwm(&servo2, pwm);
+  //   Serial.println(pwm);
+  //   delay(10);
+  // }
 }
