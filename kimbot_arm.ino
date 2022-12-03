a21f44a514415b66fde0a239e9407fa06a5375c9
DrowsyLad <tsumwashere@student.ub.ac.id>
2022-12-03 21:08:24 +0700

    Fixed Inverse and Forward Kinematics Algorithms, better testing code

--- kimbot_arm.ino
+++ kimbot_arm.ino
@@ -21,6 +21,14 @@ bool FK_debugPrint = true;
 bool IK_debugPrint = true;
 bool TEST_debugPrint = true;
 
+//coordinates for Inverse Kinematics
+float x=8, y=0, z=12;
+//angles for Forward Kinematics
+float global_angles[4] = {0,0,0,0};
+//running mode
+int global_mode = 0;
+int standby_count;
+
 float x_axis, y_axis, z_axis;
 float current_angle[4] = {0,0,0,0};
 float offset_angle[4] = {90,45,0,0};
@@ -56,6 +64,10 @@ void moveServo_pwm(int servo_id, float target_angle){
     output = servo[servo_id][max_pwm] - round(deg_to_pwm);
   else
     output = round(deg_to_pwm) + servo[servo_id][min_pwm];
+//  if(output < servo[servo_id][min_pwm])
+//    output = servo[servo_id][min_pwm];
+//  if(output > servo[servo_id][max_pwm])
+//    output = servo[servo_id][max_pwm];
   set_pwm(servo_address[servo_id], output);
   current_angle[servo_id] = target_angle;
 //  Serial.print("Servo ");
@@ -89,15 +101,11 @@ void moveAllServo_timed(int move_time, //in miliseconds
 void moveAllServo(float* target_angle){
   for(int servo_id = 0; servo_id < servo_count; servo_id++){
     moveServo_pwm(servo_id, target_angle[servo_id] + offset_angle[servo_id]);
-    // Serial.print("Servo ");
-    // Serial.print(servo_id);
-    // Serial.print(" moved to ");
-    // Serial.println(current_angle[servo_id]);
     delay(50);
   }
 }
 
-void moveToPos(double x_input, double y_input, double z_input) { 
+void moveToPos(float x_input, float y_input, float z_input) { 
   if(IK_debugPrint == true){
     Serial.println("Input Coordinates:");
     Serial.print("X: ");
@@ -107,19 +115,26 @@ void moveToPos(double x_input, double y_input, double z_input) {
     Serial.print("Z: ");
     Serial.println(z_input);
   }
-  z_input -= 4;
-  float target_angle[4];
-  target_angle[0] = atan2(y_input,z_input) * (180 / 3.1415); // base angle
-  double l = sqrt(z_input*z_input + y_input*y_input); // z_input and y_input extension
-  double h = sqrt (l*l + x_input*x_input);
-  double phi = atan(x_input/l) * (180 / 3.1415);
-  double theta = acos((h/2)/8) * (180 / 3.1415);
-  target_angle[1] = phi - theta; // angle for second part of the arm
-  target_angle[2] = phi + theta - 90; // angle for first part of the arm
-  target_angle[3] = servo4_close;
+  float target_angle[4] = {0,0,0,0};
+
+  inverseKinematics(target_angle, x_input, y_input, z_input);
+  
   moveAllServo(target_angle);
 
   float length[3] = {4, 8, 8};
+  if(TEST_debugPrint == true){
+    Serial.println("Predicted Angles:");
+    Serial.print("A1: ");
+    Serial.println(target_angle[0]);
+    Serial.print("A2: ");
+    Serial.println(target_angle[1]);
+    Serial.print("A3: ");
+    Serial.println(target_angle[2]);
+  }
+  target_angle[0] *= -1;
+  target_angle[1] *= -1;
+  target_angle[2] *= -1;
+//  target_angle[1] *= -1;
   forwardKinematics(&x_axis, &y_axis, &z_axis, target_angle, length);
   if(FK_debugPrint == true){
     Serial.println("Predicted Coordinates:");
@@ -130,22 +145,22 @@ void moveToPos(double x_input, double y_input, double z_input) {
     Serial.print("Z: ");
     Serial.println(z_axis);
   }
-//  double target_angle[0] = 1;
-//  double target_angle[1] = 2;
-//  double target_angle[2] = 3;
-  if(TEST_debugPrint == true){
-    Serial.println("Predicted Angles:");
-    Serial.print("A1: ");
-    Serial.println(target_angle[0] * RAD2DEG);
-    Serial.print("A2: ");
-    Serial.println(target_angle[1] * RAD2DEG);
-    Serial.print("A3: ");
-    Serial.println(target_angle[2] * RAD2DEG);
-  }
   // Serial.print("Phi: ");
   // Serial.println(phi);
   // Serial.print("Theta: ");
   // Serial.println(theta);
+}
+
+void inverseKinematics(float* target_angle, float x_input, float y_input, float z_input){
+  z_input -= 4;
+  target_angle[0] = - atan2(y_input,x_input) * RAD2DEG; // base angle
+  double l = sqrt(x_input * x_input + y_input * y_input); // z_input and y_input extension
+  double h = sqrt (l*l + z_input * z_input);
+  double phi = atan2(z_input,l) * RAD2DEG;
+  double theta = acos((h/2)/8) * RAD2DEG;
+  target_angle[1] = - phi - theta + 90; // angle for second part of the arm
+  target_angle[2] = - phi + theta;// - target_angle[1]; // angle for first part of the arm
+  target_angle[3] = servo4_close;
 }
 
 /*
@@ -154,27 +169,32 @@ B = Q*cos(Y)*sin(X) - R*sin(X)*sin(Y)*sin(Z) + R*cos(Y)*cos(Z)*sin(X)
 C = P + Q*sin(Y) + R*cos(Y)*sin(Z) + R*cos(Z)*sin(Y)
 */
 
-float forwardKinematics(float* x_axis, float* y_axis, float* z_axis, float* degree, float* length){
+void forwardKinematics(float* x_axis, float* y_axis, float* z_axis, float* degree, float* length){
+  degree[2] -= degree[1];
   degree[1] += 90;
   degree[2] -= 90;
   for(int servo_id = 0; servo_id < servo_count; ++servo_id){
     degree[servo_id] *= DEG2RAD;
+//    Serial.println(degree[servo_id]);
   }
   *x_axis = length[1] * cos(degree[0]) * cos(degree[1])
-          - length[2] * cos(degree[0]) * sin(degree[1]) * sin(degree[2]);
+          - length[2] * cos(degree[0]) * sin(degree[1]) * sin(degree[2])
           + length[2] * cos(degree[0]) * cos(degree[1]) * cos(degree[2]);
   *y_axis = length[1] * cos(degree[1]) * sin(degree[0])
-          - length[2] * sin(degree[0]) * sin(degree[1]) * sin(degree[2]);
+          - length[2] * sin(degree[0]) * sin(degree[1]) * sin(degree[2])
           + length[2] * cos(degree[1]) * cos(degree[2]) * sin(degree[0]);
   *z_axis = length[2] * cos(degree[1]) * sin(degree[2])
           + length[2] * cos(degree[2]) * sin(degree[1])
           + length[1] * sin(degree[1])
           + length[0];
-  degree[1] -= 90 * DEG2RAD;
-  degree[2] += 90 * DEG2RAD;
+  for(int servo_id = 0; servo_id < servo_count; ++servo_id){
+    degree[servo_id] *= RAD2DEG;
+  }
+  degree[1] -= 90;
+  degree[2] += 90;
 }
 
-float forwardKinematics_DHIYAA(float* x_axis, float* y_axis, float* z_axis, float* degree, float* length){
+void forwardKinematics_DHIYAA(float* x_axis, float* y_axis, float* z_axis, float* degree, float* length){
   *x_axis = length[1] * cos(degree[0]) * cos(degree[1]) * cos(degree[2])
           + length[1] * cos(degree[0]) * cos(degree[1])
           - length[2] * cos(degree[0]) * sin(degree[1]) * sin(degree[2]);
@@ -290,7 +310,29 @@ void testFK(float* target_angle){
   Serial.println(z_axis);
 }
 
-void testIK(){
+void testIK(float x_input, float y_input, float z_input){
+  float target_angle[4] = {0,0,0,0};
+
+  Serial.println("Input Coordinates:");
+  Serial.print("X: ");
+  Serial.println(x_input);
+  Serial.print("Y: ");
+  Serial.println(y_input);
+  Serial.print("Z: ");
+  Serial.println(z_input);
+
+  inverseKinematics(target_angle, x_input, y_input, z_input);
+  
+  Serial.println("Predicted Angles:");
+  Serial.print("A1: ");
+  Serial.println(target_angle[0]);
+  Serial.print("A2: ");
+  Serial.println(target_angle[1]);
+  Serial.print("A3: ");
+  Serial.println(target_angle[2]);
+}
+
+void test_moveToPos(){
   float x = 8, y = 0, z = 12, step = 0.1;
   int delay_ = 10;
   for(; z >= 8;){ 
@@ -320,31 +362,84 @@ void testIK(){
   }
 }
 
-void loop(){
-//  float target_angle[4] = {0,0,0,0};
-//  testFK(target_angle);
-  IK_debugPrint = false;
-  TEST_debugPrint = false;
-//  FK_debugPrint = false;
-  testIK();
-//  delay(1000);
-// test_framework_3();
-//  return;
+bool receiveXYZ(float* x, float* y, float* z){
+  if(Serial.available()){
+    *x = Serial.parseInt();
+    *y = Serial.parseInt();
+    *z = Serial.parseInt();
+    return true;
+  }
+  else return false;
 }
 
-void loop_(){
-//  moveServo_pwm(2, 0);
-//  moveServo_pwm(3, 0);
-//  delay(5000);
-//  moveServo_pwm(2, 45);
-//  moveServo_pwm(3, 45);
-//  delay(5000);
-//  moveServo_pwm(2, 90);
-//  moveServo_pwm(3, 90);
-//  delay(5000);
-//  moveServo_pwm(2, 45);
-//  moveServo_pwm(3, 45);
-//  delay(5000);
-  // moveServo(0, 0, 0, 0);
-  // delay(2000);
+bool receiveAngles(float* target_angle){
+  if(Serial.available()){
+    for(int servo_id = 0; servo_id < servo_count; ++servo_id)
+      target_angle[servo_id] = Serial.parseInt();
+    return true;
+  }
+  else return false;
+}
+
+bool receiveMode(int* mode){
+  if(Serial.available()){
+    *mode = Serial.parseInt();
+    return true;
+  }
+  else return false;
+}
+
+void loop(){
+  switch (global_mode){
+  case 0:
+    if(!receiveMode(&global_mode)){
+      Serial.println("Choose mode:");
+      Serial.println("1. Forward Kinematics");
+      Serial.println("2. Inverse Kinematics");
+      Serial.println("3. IK -> FK");
+    }
+    break;
+  
+  case 1:
+    if(receiveAngles(global_angles)){
+      float target_angle[4] = {0,0,0,0};
+      for(int i = 0; i < 4; ++i)
+        target_angle[i] = global_angles[i];
+      testFK(target_angle);
+    }
+    else{
+      Serial.println("Forward Kinematics: A1, A2, A3, A4");
+    }
+    break;
+  
+  case 2:
+    if(receiveXYZ(&x, &y, &z)){
+      testIK(x, y, z);
+    }
+    else{
+      Serial.println("Inverse Kinematics: X, Y, Z");
+    }
+    break;
+  
+  case 3:
+    if(receiveXYZ(&x, &y, &z))
+      moveToPos(x, y, z);
+    else{
+      Serial.println("Move to Coordinates: X, Y, Z");
+    }
+    break;
+  
+  default:
+    Serial.println("Mode not recognized");
+    global_mode = 0;
+    break;
+  }
+//  IK_debugPrint = false;
+//  TEST_debugPrint = false;
+//  FK_debugPrint = false;
+
+//  testIK();
+  delay(1000);
+//  test_framework_3();
+//  return;
 }
